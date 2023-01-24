37f3932b2a11d1dcd49a78026967ef3afa168de1
blueberet <63086194+Blueberet@users.noreply.github.com>
2023-01-24 10:50:52 +0700

    fix ENOENT in production

--- src/pages/api/frame/[fid].js
+++ src/pages/api/frame/[fid].js
@@ -1,8 +1,13 @@
 // send the corresponding frames file
-import fs from 'fs/promises';
-
+import fs from 'fs';
+import path from 'path';
 export default async (req, res) => {
     const { fid } = req.query;
-    const file = await fs.readFile(`public/frames/${fid}.json`);
+    let framedir = path.resolve('./public', 'frames')
+
+    let filepath = path.join(framedir, fid + '.json');
+
+    let file = fs.readFileSync(filepath, 'utf8')
+
     res.status(200).json(JSON.parse(file));
 }
